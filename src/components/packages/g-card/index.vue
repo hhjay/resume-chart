2e79c1b21b18f5182f28c3ca94c4d68fac2658db
huijie <huijie@hudongpai.com>
2018-12-25 18:04:52 +0800

    card组件开发及使用

--- src/components/packages/g-card/index.vue
+++ src/components/packages/g-card/index.vue
@@ -1,11 +1,18 @@
 <template>
-    <div>
-        卡片
+    <div class="h-card">
+        <div class="h-card__header" v-if="this.$slots.header">
+            <!-- 具名slot -->
+            <slot name="header"></slot>
+        </div>
+        <div class="h-card__body">
+            <!-- 默认slot -->
+            <slot></slot>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "g-card"
+        name: "g-card",
     }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,15 @@
 <template>
-	<div class="home">
-		<!-- 
-			<img alt="Vue logo" src="../assets/logo.png">
-			<HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> 
-		-->
-		
-		home
+	<div class="g-contain home">
+		<g-card>
+			<!-- <span slot="header">title</span> -->
+			test body
+		</g-card>
 	</div>
 </template>
 <script lang="ts">
-	// import Vue from 'vue';
+	import Vue from 'vue';
 	// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-	// export default Vue.extend({
-	// 	name: 'home',
-	// 	components: {
-	// 		HelloWorld,
-	// 	},
-	// });
-
-	export default {
+	export default Vue.extend({
 		name: 'home',
-	}
+	});
 </script>
