499f59f0475927a4eeb327e2b5ee82e12f37dd87
huijie <huijie@hudongpai.com>
2018-12-28 18:18:00 +0800

    项目经历

---
+++ src/views/home/components/project-exp.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="work-exp">
+        项目经验111
+    </div>
+</template>
+<script lang="ts">
+    import Vue from 'vue'
+    export default Vue.extend({
+        
+    })
+</script>
+
